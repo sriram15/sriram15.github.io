cca018ed311d47c44d386cab53f34ff95651b662
sriram15 <sriram.thiagarajan94@gmail.com>
2017-07-05 00:12:12 +0530

    trying out various options for the header image

--- portfolio/src/app/home/home.component.ts
+++ portfolio/src/app/home/home.component.ts
@@ -9,9 +9,26 @@ import { ProjectTileComponent } from "app/project-tile/project-tile.component";
 })
 export class HomeComponent implements OnInit {
 
+  private showImg = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  mouseEnter(ev){
+    var imgWidth = ev.srcElement.clientWidth;
+    var cursorX = ev.clientX;
+    var percent = (cursorX/imgWidth) * 100;
+    console.log(percent);
+    if(percent > 26 && percent < 30){
+      this.showImg = true;
+    }else{
+      this.showImg = false;
+    }
+  }
+
+  mouseLeave(){
+    this.showImg = false;
   }
 
 }
